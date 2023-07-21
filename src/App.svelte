a32cf833989fcb42815ebcef98896600a20c74ff
venkatesh c <venkat@print2block.com>
2023-07-22 00:03:54 +0530

    bg-added

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,17 @@
+<script>
+  import Header from "./Pages/header/index.svelte"
+</script>
 
-<h1 class="text-red-600 text-7xl">Hello World</h1>
+<main class="flex h-screen w-screen bg-cover overflow-hidden">
+  <Header />
+</main>
+
 <style global lang="postcss">
   @tailwind base;
   @tailwind components;
   @tailwind utilities;
+
+  main {
+    background: url(/assets/bg-blured.png) no-repeat bottom center;
+  }
 </style>
